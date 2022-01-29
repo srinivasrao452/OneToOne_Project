ebbf05200561eb23e6cc3f09aba7a8711233b1ed
srinivasrao <srinivasrao452@gmail.com>
2022-01-29 07:33:26 +0530

    First Commit

---
+++ OneToOne_App/admin.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from OneToOne_App.models import Student,Course
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'sname','marks','address']
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id','cname','cfee']
+
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Course,CourseAdmin)
---
+++ OneToOne_App/models.py
@@ -0,0 +1,20 @@
+
+from django.db import models
+
+class Student(models.Model):
+    sname = models.CharField(max_length=30)
+    marks = models.IntegerField()
+    address = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.sname
+
+
+class Course(models.Model):
+    cname = models.CharField(max_length=30)
+    cfee = models.IntegerField()
+
+    student = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return self.cname
